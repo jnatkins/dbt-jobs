7fc176007ea88119ac5d4dab04c787ff187244e5
Jon Natkins <jon.natkins@dbtlabs.com>
2021-08-17 08:09:44 -0700

    Moving to a title-based analysis away from overall tech co-presentation

--- models/marts/job_descriptions__summarize_tech.sql
+++ models/marts/job_descriptions__summarize_tech.sql
@@ -5,7 +5,10 @@
         'bigquery','redshift','fivetran','spark','postgres','java','kafka',
         'talend','docker','kubernetes','gitlab','github','git','databricks',
         'hadoop','matillion','powerbi','mysql','informatica','pandas','dagster',
-        'jupyter','scikit','bitbucket','confluent','cloudera','jinja' 
+        'jupyter','scikit','bitbucket','confluent','cloudera','jinja','scala','metabase',
+        'superset','streaming','batch','aws','google','azure','nosql','influence','analysis',
+        'stakeholder','insight','align','test','develop','share','collaborate','notebook',
+        'prepare','curate','maintain','architect','design'
     ] 
 %}
 
@@ -15,21 +18,40 @@ with stg_job_descriptions as (
 
 ),
 
+job_postings as (
+
+    select job_id, cleaned_title from {{ ref('job_postings__clean_titles') }}
+
+),
+
+joined as (
+
+    select 
+        p.cleaned_title,
+        jd.job_description
+    from stg_job_descriptions jd join job_postings p
+        on (jd.job_id = p.job_id)
+
+),
+
 count_technologies as (
     select
+        cleaned_title,
         count(*) as total_cnt,
     {% for tech in tech_list %}
-        sum((job_description ilike '%{{ tech }}%')::int) as {{ tech }}_cnt{% if not loop.last %},{% endif %}
+        sum((job_description ilike '%{{ tech }}%')::int) as "{{ tech }}_cnt"{% if not loop.last %},{% endif %}
     {% endfor %}
 
-    from stg_job_descriptions
+    from joined
+    group by cleaned_title
 
 ),
 
 pctage_technologies as (
     select
+        cleaned_title,
     {% for tech in tech_list %}
-        round(({{ tech }}_cnt::numeric / total_cnt::numeric) * 100.0,1) as {{ tech }}{% if not loop.last %},{% endif %}
+        round(("{{ tech }}_cnt"::numeric / total_cnt::numeric) * 100.0,1) as "{{ tech }}"{% if not loop.last %},{% endif %}
     {% endfor %}
 
     from count_technologies
